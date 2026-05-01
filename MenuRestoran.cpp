4836a4de8cdfb0588d2632d9d07a1e6bad33160e
Martzz-a <maritzafidelyah@gmail.com>
2026-05-01 16:22:38 +0700

    Membuat fungsi addNode dan menambahkan proses input data namaMenu serta alokasi memori

--- MenuRestoran.cpp
+++ MenuRestoran.cpp
@@ -20,4 +20,17 @@ public:
     {
         START = NULL;
     }
+
+    void addNode()
+    {
+        string addNamaMenu;
+        cout << "\nMasukkan nama menu: ";
+        cin >> addNamaMenu;
+
+        // Step 1: Mengalokasi memori untuk new node
+        Node *newNode = new Node();
+
+        // Step 2: Mengisi data di data fields
+        newNode->namaMenu = addNamaMenu;
+    }
 };
